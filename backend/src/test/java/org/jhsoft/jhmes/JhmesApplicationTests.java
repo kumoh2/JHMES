73279c51f5a622bf5f1f900bc4e98129804cd62c
Choi Jaeho <31762986+kumoh2@users.noreply.github.com>
2025-04-20 07:28:38 +0900

    스프링부트 프로젝트 설치
    그래들->메이븐 으로 변경

---
+++ backend/src/test/java/org/jhsoft/jhmes/JhmesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jhsoft.jhmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
