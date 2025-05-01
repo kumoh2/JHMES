d19c267328828d140c16a1c92fe7c6dfa4c8c8a6
Choi Jaeho <31762986+kumoh2@users.noreply.github.com>
2025-05-01 12:11:52 +0900

    스프링부트 최소 설치

---
+++ backend/src/main/java/org/jhsoft/jhmes/JhmesApplication.java
@@ -0,0 +1,13 @@
+package org.jhsoft.jhmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhmesApplication.class, args);
+	}
+
+}
